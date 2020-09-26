86f7a59056a0b336f89f5390d8d133d73ae35d36
ibrahimmohammed1002@gmail.com <ibrahimmohammed1002@gmail.com>
2020-09-26 14:32:14 +0200

    updatesAfter3rdpatch

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -2,8 +2,8 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 export default function (context) {
 	return {
 		httpLinkOptions: {
-			// uri: 'https://atlas0.herokuapp.com/v1/graphql',
-			uri: 'http://localhost:8080/v1/graphql',
+			uri: 'https://atlas0.herokuapp.com/v1/graphql',
+			// uri: 'http://localhost:8080/v1/graphql',
 			credentials: 'same-origin'
 		},
 		cache: new InMemoryCache(),
