08bb9a55a4589ad2aa5812a24f0e66c31e324f2e
Ibrahim Mohammed <ibrahimmohammed1002@gmail.com>
2022-07-12 07:01:05 +0200

    updating server

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -2,8 +2,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 export default function(context) {
   return {
     httpLinkOptions: {
-      // uri: 'https://atlas0.herokuapp.com/v1/graphql',
-      uri: "https://oaxkhlbbctdgnpylqpfe.nhost.run/v1/graphql",
+      uri: "https://ppsqorhnlmuckkwxtoxh.nhost.run/v1/graphql",
       // uri: 'http://localhost:8080/v1/graphql',
       credentials: "same-origin"
     },
