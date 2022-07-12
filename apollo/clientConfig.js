b38f02d5f07d8e74d0d02d730ca0fa9da0b049de
Ibrahim Mohammed <ibrahimmohammed1002@gmail.com>
2022-07-12 06:30:26 +0200

    changing server

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -1,11 +1,12 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
-export default function (context) {
-	return {
-		httpLinkOptions: {
-			uri: 'https://atlas0.herokuapp.com/v1/graphql',
-			// uri: 'http://localhost:8080/v1/graphql',
-			credentials: 'same-origin'
-		},
-		cache: new InMemoryCache(),
-	}
+export default function(context) {
+  return {
+    httpLinkOptions: {
+      // uri: 'https://atlas0.herokuapp.com/v1/graphql',
+      uri: "https://oaxkhlbbctdgnpylqpfe.nhost.run/v1/graphql",
+      // uri: 'http://localhost:8080/v1/graphql',
+      credentials: "same-origin"
+    },
+    cache: new InMemoryCache()
+  };
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,47 @@
-import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
-import webpack from 'webpack'
+import AntdDayjsWebpackPlugin from "antd-dayjs-webpack-plugin";
+import webpack from "webpack";
 
 export default {
   // mode: "universal",
-  target: 'static',
-    router: {
+  target: "static",
+  router: {
     base:
-      process.env.NODE_ENV === "development" ? process.env.BASE_URL : "/beta/dist/"
+      process.env.NODE_ENV === "development"
+        ? process.env.BASE_URL
+        : "/beta/dist/"
   }, // where <subfolder> is the subfolder!
   /*
-** Headers of the page
-*/
+   ** Headers of the page
+   */
   generate: {
-    fallback: '404.html'
+    fallback: "404.html",
+    concurrency: 500,
+    interval: 1000
   },
   head: {
-    title: 'DIGITAL ATLAS OF MICRONESIA',
+    title: "DIGITAL ATLAS OF MICRONESIA",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The Digital Atlas of Micronesia is the foremost source of maps and geospatial information for the Federated States of Micronesia. People searching for any sort of geospatial information can come to this web-based Atlas to identify, visualize, query, analyze, and acquire datasets relevant to their interests. At the core of the Atlas are web-enabled GIS and searchable database that allow users to view and manipulate hundreds of layers of geospatial information. In addition to serving as a data repository, the Atlas is also an informational tool and incorporates hundreds of pre-made maps, charts, and datasets related to the natural and human environment of Micronesian islands. Outlined below are some of the key features of this most comprehensive geospatial data source available for the Federated States of Micronesia. Flip the cards for additional information.' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "The Digital Atlas of Micronesia is the foremost source of maps and geospatial information for the Federated States of Micronesia. People searching for any sort of geospatial information can come to this web-based Atlas to identify, visualize, query, analyze, and acquire datasets relevant to their interests. At the core of the Atlas are web-enabled GIS and searchable database that allow users to view and manipulate hundreds of layers of geospatial information. In addition to serving as a data repository, the Atlas is also an informational tool and incorporates hundreds of pre-made maps, charts, and datasets related to the natural and human environment of Micronesian islands. Outlined below are some of the key features of this most comprehensive geospatial data source available for the Federated States of Micronesia. Flip the cards for additional information."
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: {
-    color: 'red', height: '3px'
+    color: "red",
+    height: "3px"
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     {
       src: "@/assets/css/antd.less",
@@ -48,83 +56,82 @@ export default {
     // { src: 'https://code.jquery.com/jquery-3.5.1.min.js' }
   ],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    '@/plugins/antd-ui',
+    "@/plugins/antd-ui",
     {
-      src: '@/plugins/parallax',
-      mode: 'client'
+      src: "@/plugins/parallax",
+      mode: "client"
     },
     {
-      src: '@/plugins/slick.min',
-      mode: 'client'
-    },
+      src: "@/plugins/slick.min",
+      mode: "client"
+    }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   // buildDir: 'dist',
-  buildModules: [
-  ],
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/pwa',
-    '@nuxtjs/apollo',
-    'nuxt-webfontloader',
-    'vue-scrollto/nuxt',
-    '@nuxtjs/axios',
+    "@nuxtjs/pwa",
+    "@nuxtjs/apollo",
+    "nuxt-webfontloader",
+    "vue-scrollto/nuxt",
+    "@nuxtjs/axios"
   ],
   // Give apollo module options
   apollo: {
     cookieAttributes: {
-      expires: 7, // optional, default: 7 (days)
+      expires: 7 // optional, default: 7 (days)
     },
     includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
-    authenticationType: 'Bearer', // optional, default: 'Bearer'
+    authenticationType: "Bearer", // optional, default: 'Bearer'
     // optional
     // errorHandler: '~/plugins/apollo-error-handler.js',
     // required
     clientConfigs: {
-      default: '~/apollo/clientConfig.js'
+      default: "~/apollo/clientConfig.js"
     }
   },
   webfontloader: {
     google: {
-      families: ['Open Sans:400'] //Loads Lato font with weights 400 and 700
+      families: ["Open Sans:400"] //Loads Lato font with weights 400 and 700
     }
   },
   /*
-** Axios module configuration
-** See https://axios.nuxtjs.org/options
-*/
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   router: {
-    linkExactActiveClass: 'exact-active-link'
+    linkExactActiveClass: "exact-active-link"
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     analyze: false,
     extractCSS: false,
+
     extend(config, ctx) {
       // ctx.loaders.less.javascriptEnabled = true;
-      config.resolve.alias['@ant-design/icons/lib/dist$'] = '~/icons.js'
+      config.resolve.alias["@ant-design/icons/lib/dist$"] = "~/icons.js";
     },
     loaders: {
       less: {
         javascriptEnabled: true
       },
-      'html-loader': 'html-loader'
+      "html-loader": "html-loader"
     },
     plugins: [
       // global modules
@@ -132,10 +139,10 @@ export default {
         // replaceMoment: true
       }),
       new webpack.ProvidePlugin({
-        jQuery: 'jquery',
-        $: 'jquery',
-        'window.jQuery': 'jquery',
-      }),
+        jQuery: "jquery",
+        $: "jquery",
+        "window.jQuery": "jquery"
+      })
     ]
   }
-}
+};
